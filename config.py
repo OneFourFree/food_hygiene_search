e42eeee32d295d9bf7317edb5abbcdcbc3057daa
Dean Robinson <dean.robinson@prospr.biz>
2018-11-19 14:38:31 +0000

    config file

---
+++ config.py
@@ -0,0 +1,26 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+# Create .env file path.
+dotenv_path = join(dirname(__file__), '.env')
+
+# Load file from the path.
+load_dotenv(dotenv_path)
+
+# Grabs the folder where the script runs.
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+    # Enable debug mode.
+    DEBUG = True
+
+    # Secret key for session management. You can generate random strings here:
+    SECRET_KEY = os.getenv('SECRET_KEY')
+
+# Development environment settings
+class DevelopmentConfig(Config):
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
+    DEBUG = False
+    PORT = 5006
