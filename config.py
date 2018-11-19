ea6ef3418326ee7733c74ebf5927f4e1243221f7
Dean Robinson <dean.robinson@prospr.biz>
2018-11-19 15:36:49 +0000

    Update config.py

--- config.py
+++ config.py
@@ -27,6 +27,6 @@ class DevelopmentConfig(Config):
 # Development environment settings
 class ProductionConfig(Config):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = os.environ.get('CLEARDB_DATABASE_URL', None)
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI', None)
     DEBUG = False
     SECRET_KEY = os.environ.get('SECRET_KEY', None)
